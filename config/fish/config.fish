8ac17d3ea4a59b040476d6f393c8e1bd9b53a994
Dorian Marié <dorian@thoughtbot.com>
2022-09-01 08:05:27 +0000

    simple and compatible with fish 2 fish config file

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,26 +9,12 @@ set -x VISUAL vim
 set -x HOMEBREW_NO_ANALYTICS 1
 set -x HOMEBREW_NO_ENV_HINTS 1
 
-fish_add_path "$HOME/.bin"
-fish_add_path "$HOME/.cargo/bin"
-fish_add_path /usr/local/sbin
-fish_add_path ".git/safe/../../bin:$PATH"
+set -U fish_user_paths "$HOME/.bin" $fish_user_paths
+set -U fish_user_paths /usr/local/sbin $fish_user_paths
+set -U fish_user_paths ".git/safe/../../bin:$PATH" $fish_user_paths
 
 alias xvim "xargs -o vim"
 alias be "bundle exec"
 alias glg "git ls-files | grep"
-
 alias ll "ls -lah"
 alias ln "ln -v"
-alias mkdir "mkdir -p"
-alias b "bundle"
-alias migrate "bin/rails db:migrate db:rollback && bin/rails db:migrate db:test:prepare"
-alias s "rspec"
-alias path 'echo $PATH | tr -s ":" "\n"'
-alias e "$EDITOR"
-alias v "$VISUAL"
-alias c "crystal"
-alias cf "crystal tool format"
-alias cs "crystal spec"
-alias cb "crystal build"
-alias cr "crystal run"
