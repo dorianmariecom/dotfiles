6d606130ed8523f811c250e1c1f901c9ffa81835
Dorian Marié <dorian@dorianmarie.com>
2024-02-21 18:58:26 +0100

    upgrade bundler

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,20 +11,25 @@ function fish_prompt
 end
 
 set -x EDITOR vim
+set -x OBJC_DISABLE_INITIALIZE_FORK_SAFETY YES
 set -x VISUAL vim
 
 fish_add_path $HOME/.bin
-fish_add_path /usr/local/sbin
 fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/sbin
+fish_add_path /usr/local/sbin
 
-alias xvim "xargs -o vim"
 alias be "bundle exec"
+alias bootstrap "bin/bootstrap"
+alias console "kamal app exec -i 'bin/rails console'"
+alias deploy "bin/deploy"
+alias dev "bin/dev"
 alias glg "git ls-files | grep"
 alias ll "ls -lah"
 alias ln "ln -v"
-alias rake "bundle exec rake"
 alias rails "bundle exec rails"
+alias rake "bundle exec rake"
 alias rspec "bundle exec rspec"
+alias xvim "xargs -o vim"
 
 source ~/.asdf/asdf.fish
