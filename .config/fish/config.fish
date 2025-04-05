5806db435494c804b292bcd388111a5841cc7757
Dorian Marié <dorian@dorianmarie.com>
2025-04-05 09:07:57 +0200

    updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,18 +10,9 @@ function fish_prompt
   )
 end
 
-set -x ANDROID_HOME $HOME/Library/Android/sdk
 set -x EDITOR vim
-set -x GPG_TTY (tty)
-set -x JAVA_HOME /Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
-set -x OBJC_DISABLE_INITIALIZE_FORK_SAFETY YES
-set -x PNPM_HOME $HOME/Library/pnpm
 set -x VISUAL vim
 
-fish_add_path $ANDROID_HOME/emulator
-fish_add_path $ANDROID_HOME/platform-tools
-fish_add_path $HOME/.bin
-fish_add_path $HOME/Library/pnpm
 fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/sbin
 fish_add_path /usr/local/sbin
@@ -29,4 +20,4 @@ fish_add_path /usr/local/sbin
 alias gist "gh gist create"
 alias xvim "xargs -o vim"
 
-source ~/.asdf/asdf.fish
+set -gx --prepend PATH "$HOME/.asdf/shims"
